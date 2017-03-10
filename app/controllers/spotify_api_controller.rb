9c757da0d54815052fff8eebd05fce815e4672f8
jef4490 <jeffrey.hechler@gmail.com>
2017-03-10 11:34:59 -0500

    add api logic, search views

--- app/controllers/spotify_api_controller.rb
+++ app/controllers/spotify_api_controller.rb
@@ -1,9 +1,15 @@
 class SpotifyApiController < ApplicationController
 
   def search
-    binding.pry
-    SpotifyApi.new(spotify_params)
-    
+    # binding.pry
+    search_obj = SpotifyApi.new(spotify_params[:query])
+    x = search_obj.search
+    if x.length > 0
+      @songs = search_obj.make_songs
+      render 'search_results'
+    else
+
+    end
   end
 
   private
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,13 @@
 class Song < ApplicationRecord
   has_many :reviews
   has_many :accounts, through: :reviews
+
+  def song_data=(song_data)
+    self.name = song_data["name"]
+    self.artist_name = song_data["artists"].map{|artist| artist["name"]}.join(", ")
+    self.play_url = song_data["external_urls"]["spotify"]
+    self.spotify_song_id = song_data["id"]
+    self.album_name = song_data["album"]["name"]
+  end
+
 end
