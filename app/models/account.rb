05ec702e7c4919e775e6f37460aa1caa7ed029a5
jef4490 <jeffrey.hechler@gmail.com>
2017-03-11 17:54:00 -0500

    complete account vote relationship

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,13 +11,31 @@ class Account < ApplicationRecord
     self.reviews.map{|review| review.vote_sum}.sum
   end
 
+####################################################
+
   #account having voted on a review
   def voted?(review_id)
-    binding.pry
-    Vote.where(account_id: self.id).where(review_id: review_id)
+    !Vote.where(account_id: self.id).where(review_id: review_id).empty?
   end
 
-  ### Accounts you've upvoted alot
+  #the value of the vote you cast for a given review
+  def voted_score(review_id)
+    Vote.where(account_id: self.id).where(review_id: review_id).first.score
+  end
+
+  #returns the votes that you cast
+  def votes_cast
+    Vote.where(account_id: self.id)
+  end
+
+  ### Accounts you've upvoted
+  def voted_accounts
+    Account.joins(:votes).where(votes: {account_id: current_user.id})
+  end
+
+  def upvoted_accounts
+    Account.joins(:votes).where(votes: {account_id: 13}).where(votes: {score: 1})
+  end
 
   ### Reviews_I_have_voted
 
