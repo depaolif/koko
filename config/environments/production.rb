36ec453e9be6e2124bb1a2eb5811abc26d623a09
jef4490 <jeffrey.hechler@gmail.com>
2017-03-13 18:26:24 -0400

    websockets stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://kokomusic.herokuapp.com/cable"
+  
+  config.action_cable.allowed_request_origins = ['https://kokomusic.herokuapp.com', 'http://kokomusic.herokuapp.com']
+
 end
