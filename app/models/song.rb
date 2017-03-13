271f7a538e1043a002b0e99c12b20b400ea08682
github username <github email address>
2017-03-13 11:52:23 -0400

    fixed add/remove friend bug from show page, added delete review from profile view (if your profile), made it show first artist instead of last

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,13 +4,15 @@ class Song < ApplicationRecord
   belongs_to :artist
 
   def song_data=(song_data)
+    artists = []
     self.name = song_data["name"]
     self.play_url = song_data["uri"]
     self.spotify_song_id = song_data["id"]
     self.album_name = song_data["album"]["name"]
     all_artists(song_data).each do |artist_name|
-      self.artist = Artist.find_or_create_by(name: artist_name)
+      artists << Artist.find_or_create_by(name: artist_name)
     end
+    self.artist = artists[0]
   end
 
   def all_artists(song_data)
