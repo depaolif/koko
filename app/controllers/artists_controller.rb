b8d59e35ab3ba2db07c68170dfead0291ff59978
jef4490 <jeffrey.hechler@gmail.com>
2017-03-14 09:59:27 -0400

    home page now displays influencer's picks properly

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -20,7 +20,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.find_by(id: params[:id])
     @artist_songs_ordered_by_review = {}
     @artist.songs.each do |song|
-      if song.reviews.count > 0 
+      if song.reviews.count > 0
         @artist_songs_ordered_by_review[song] = song.weighted_review_average.round(2)
       end
     end
