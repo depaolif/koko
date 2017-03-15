309cdb283a034ec07182ae3d6847ab8dadb7eada
github username <github email address>
2017-03-15 09:34:44 -0400

    emergency fixes

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ApplicationRecord
     self.spotify_song_id = song_data["id"]
     self.album_name = song_data["album"]["name"]
     all_artists(song_data).each do |artist_name|
-      artists << artist_name
+      artists << Artist.find_or_create_by(name: artist_name)
     end
     self.artist = artists[0]
   end
