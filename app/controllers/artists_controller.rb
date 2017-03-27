f17cb67709dac72bc51d2ed2347c50cca4b4ec31
jef4490 <jeffrey.hechler@gmail.com>
2017-03-27 12:00:04 -0400

    artist show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,19 +4,21 @@ class ArtistsController < ApplicationController
   end
 
   def trending
-    @trending_artists = Artist.trending
+    @trending_artists = Artist.trending_artists
     render :trending
   end
 
   def show
     @artist = Artist.find_by(id: params[:id])
-    @artist_songs_ordered_by_review = {}
-    @artist.songs.each do |song|
-      if song.reviews.count > 0
-        @artist_songs_ordered_by_review[song] = song.weighted_average.round(2)
-      end
-    end
-    @artist_songs_ordered_by_review = Hash[@artist_songs_ordered_by_review.sort_by{|k, v| v}.reverse]
+    @artist_songs_ordered_by_review = @artist.trending_songs
+
+    # @artist_songs_ordered_by_review = {}
+    # @artist.songs.each do |song|
+    #   if song.reviews.count > 0
+    #     @artist_songs_ordered_by_review[song] = song.weighted_average.round(2)
+    #   end
+    # end
+    # @artist_songs_ordered_by_review = Hash[@artist_songs_ordered_by_review.sort_by{|k, v| v}.reverse]
   end
 
 end
