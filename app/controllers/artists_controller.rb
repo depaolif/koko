36868d99c1a16222b48284944f2067b4b7771a73
jef4490 <jeffrey.hechler@gmail.com>
2017-03-27 12:46:34 -0400

    removed unused code

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,14 +11,6 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find_by(id: params[:id])
     @artist_songs_ordered_by_review = @artist.trending_songs
-
-    # @artist_songs_ordered_by_review = {}
-    # @artist.songs.each do |song|
-    #   if song.reviews.count > 0
-    #     @artist_songs_ordered_by_review[song] = song.weighted_average.round(2)
-    #   end
-    # end
-    # @artist_songs_ordered_by_review = Hash[@artist_songs_ordered_by_review.sort_by{|k, v| v}.reverse]
   end
 
 end
