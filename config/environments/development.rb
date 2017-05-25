3ab3c288ce4a1278f777fb3b0fa52909f43d1c7b
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-25 19:47:04 -0400

    removed bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,13 +54,5 @@ Rails.application.configure do
 
   config.action_cable.url = "ws://localhost:3000/cable"
 
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.growl = false
-    Bullet.rails_logger = true
-  end
 
 end
